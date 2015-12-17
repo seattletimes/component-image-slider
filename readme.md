a53e08dcb670e4ef2d03814aef5273f19bbc89a4
Audrey Carlsen <acarlsen@seattletimes.com>
2015-12-17 15:11:55 -0800

    hello world!

---
+++ readme.md
@@ -0,0 +1,14 @@
+component-image-slider
+====================
+
+Quickly generate an HTML slider to easily overlay and compare two images. Example of usage:
+
+```
+  <image-slider style="width: 300px">
+    <img src="http://placekitten.com/g/300/450">
+    <img src="http://placehold.it/300x450">
+  </image-slider>
+```
+
+`<image-slider>` was built using the Seattle Times' [web component
+template](https://github.com/seattletimes/component-template).
---
+++ src/image-slider/image-slider.js
@@ -0,0 +1,67 @@
+//shim
+require("document-register-element");
+
+//element setup
+var template = require("./_template.html");
+require("./image-slider.less");
+
+var proto = Object.create(HTMLElement.prototype);
+
+proto.createdCallback = function() {
+
+  var images = this.querySelectorAll("img");
+  this.innerHTML = template();
+  this.querySelector(".first").appendChild(images[0]);
+  this.querySelector(".second").appendChild(images[1]);
+  images[0].setAttribute("draggable", "false");
+  images[1].setAttribute("draggable", "false");
+
+  var first = this.querySelector(".first");
+  var container = this;
+
+  var resizeable = function(e) {
+    e.preventDefault();
+    var bounds = e.target.getBoundingClientRect();
+    var x;
+    if (e.touches) { 
+      x = e.touches[0].pageX - bounds.left;
+    } else { 
+      x = e.pageX - bounds.left;
+    }
+    first.style.width = x + "px";
+  };
+
+  first.addEventListener("mousedown", function(e) {
+    e.preventDefault();
+    container.addEventListener("mousemove", resizeable);
+  });
+  first.addEventListener("touchstart", function(e) {
+    e.preventDefault();
+    container.addEventListener("touchmove", resizeable);
+  });
+  container.addEventListener("mouseup", function() {
+    container.removeEventListener("mousemove", resizeable);
+  });
+  container.addEventListener("touchend", function() {
+    container.removeEventListener("touchmove", resizeable);
+  });
+
+  var resize = function() {
+    var width = container.getBoundingClientRect().width;
+    container.querySelector(".first img").style.maxWidth = width + "px";
+  };
+  
+  resize();
+  window.addEventListener('resize', resize);
+
+};
+
+proto.attachedCallback = function() {};
+proto.detachedCallback = function() {};
+proto.attributeChangedCallback = function() {};
+
+try {
+  document.registerElement("image-slider", { prototype: proto });
+} catch (e) {
+  console.error("<image-slider> has already been registered.")
+}
